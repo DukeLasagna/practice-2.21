10b20fb04b2c1f16a68649a606f793a6b0ee37c8
junklasagna <redlinetango@gmail.com>
2022-03-19 12:28:54 +0800

    upload files

---
+++ practice 2.21/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int linecounter;
+    linecounter = 1;
+
+    if (linecounter = 1)
+    {
+        cout << "*********" << endl;
+          linecounter = linecounter + 1;
+    }
+
+    while ( linecounter >= 2 && linecounter <= 8 )
+    {
+        cout << "*       *" << endl;
+        linecounter = linecounter + 1;
+    }
+
+    if ( linecounter = 9 )
+    {
+        cout << "*********" << endl;
+    }
+    return 0;
+}
